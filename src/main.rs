ef3a2e24739573281c432b6cd7f89f6ee273b98f
KKonaOG <kaf386@msstate.edu>
2024-02-22 08:09:52 -0600

    Working on layout

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,77 @@
-use iced::widget::{button, column, text};
-use iced::{Alignment, Element, Sandbox, Settings};
-
+use iced::executor;
+use iced::widget::container::Appearance;
+use iced::{Application, Command, Element, Settings, Theme, Alignment, Length, theme};
+use iced::widget::{container, column, row, horizontal_space, scrollable, button, text};
 pub fn main() -> iced::Result {
-    Counter::run(Settings::default())
+    Saage::run(Settings::default())
 }
 
-struct Counter {
-    value: i32,
-}
+struct Saage;
 
-#[derive(Debug, Clone, Copy)]
-enum Message {
-    IncrementPressed,
-    DecrementPressed,
-}
+impl Application for Saage {
+    type Executor = executor::Default;
+    type Flags = ();
+    type Message = ();
+    type Theme = Theme;
 
-impl Sandbox for Counter {
-    type Message = Message;
-
-    fn new() -> Self {
-        Self { value: 0 }
+    fn new(_flags: ()) -> (Saage, Command<Self::Message>) {
+        (Saage, Command::none())
     }
 
     fn title(&self) -> String {
-        String::from("Counter - Iced")
+        String::from("Saage")
     }
 
-    fn update(&mut self, message: Message) {
-        match message {
-            Message::IncrementPressed => {
-                self.value += 1;
-            }
-            Message::DecrementPressed => {
-                self.value -= 1;
-            }
-        }
+    fn update(&mut self, _message: Self::Message) -> Command<Self::Message> {
+        Command::none()
     }
 
-    fn view(&self) -> Element<Message> {
-        column![
-            button("Increment").on_press(Message::IncrementPressed),
-            text(self.value).size(50),
-            button("Decrement").on_press(Message::DecrementPressed)
-        ]
-        .padding(20)
-        .align_items(Alignment::Center)
-        .into()
+    fn view(&self) -> Element<Self::Message> {
+        let header = container(
+            row![
+                horizontal_space(),
+                text(self.title()),
+                horizontal_space(),
+            ]
+            .padding(10)
+            .align_items(Alignment::Center),
+        )
+        .style(|theme: &Theme| {
+            let palette = theme.extended_palette();
+        
+            container::Appearance::default()
+                .with_border(palette.background.strong.color, 1).with_background(palette.primary.weak.color)
+        });
+        
+        let navigation = container(
+            column![button("Search").width(Length::Fill), button("Tracking").width(Length::Fill), button("Settings").width(Length::Fill), button("About").width(Length::Fill)]
+                .spacing(20)
+                .padding(10)
+                .width(200)
+                .align_items(Alignment::Center),
+        )
+        .style(theme::Container::Box)
+        .height(Length::Fill);
+        
+        let content = container(
+            scrollable(
+                column![
+                    "Content!",
+                    "The end"
+                ]
+                .spacing(40)
+                .align_items(Alignment::Center)
+                .width(Length::Fill),
+            )
+            .height(Length::Fill),
+        )
+        .padding(10);
+        
+        column![header, row![navigation, content]].into()
     }
+
 }
+
+
+
+
